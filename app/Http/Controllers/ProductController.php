20ba04d6c133c2e66d67ac2814b3c19f581cb5d1
Md. Kamrul Hasan <kamrul@pathao.com>
2018-03-27 16:48:21 +0600

    Product Delete Implemented

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -102,6 +102,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response(['data' => 'Deleted successfully.'], 204);
     }
 }
